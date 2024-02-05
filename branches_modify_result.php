ac4df0ad64b4d04fc903f1490b9d5f8e03e05844
Xin Feng <126309503+DanielF2002@users.noreply.github.com>
2024-02-05 16:51:53 +0200

    bug fix

--- branches_modify_result.php
+++ branches_modify_result.php
@@ -3,6 +3,6 @@ $pageDescription = "The result of the modification of branch";
 $pageAdmin = true;
 include "header.php"; ?>
 <main>
-    <h1>Result</h1>
+    <h1>Result</h1> 
 </main>
 <?php include "footer.php"; ?>
