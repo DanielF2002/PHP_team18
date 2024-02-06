8e00a53afab2b294371d037df303799784aecbf6
Xin Feng <126309503+DanielF2002@users.noreply.github.com>
2024-02-06 16:45:54 +0200

    Refactor for better abstraction on feature 1-3

--- feature3_result.php
+++ feature3_result.php
@@ -1,33 +1,37 @@
 <?php $pageTitle = "Branches Management";
 $pageDescription = "Manage the branches";
 $pageAdmin = true;
+
 if (isset($_POST["mode"])) { // Only show page when mode is set.
     include "layout/header.php";
-    switch ($_POST["mode"]) { // Assemble SQL query by mode.
-        case "insert":
-            $branchSql = "INSERT INTO xin_feng_branches (name, tel, email, address, url) 
-            VALUES ('" . $_POST["name"] . "', '" . $_POST["tel"] . "', '" . $_POST["email"] . "', 
-            '" . $_POST["address"] . "', '" . $_POST["url"] . "')";
-            break;
-        case "update":
-            $branchSql = "UPDATE xin_feng_branches SET name='" . $_POST["name"] . "', tel='" . $_POST["tel"] . "', email='" . $_POST["email"] . "', 
-            address='" . $_POST["address"] . "', url='" . $_POST["url"] . "' WHERE id='" . $_POST["id"] . "'";
-            break;
-        case "delete":
-            $branchSql = "DELETE FROM xin_feng_branches WHERE id='" . $_POST["id"] . "'";
-            break;
-        default:
-            return;
-    }
+    $branchSql = branchAssembleSql();
     // Run SQL and show the result.
-    if (isset($branchSql)) {
-        branchRunSql($branchSql);
+    if (isset($branchSql) || !empty($branchSql)) {
+        branchRunSqlAndEcho($branchSql);
     }
     include "layout/footer.php";
 }
 
+// Assemble sql by request mode.
+function branchAssembleSql() {
+    global $_POST;
+    switch ($_POST["mode"]) { // Assemble SQL query by mode.
+        case "insert":
+            return "INSERT INTO xin_feng_branches (name, tel, email, address, url) 
+            VALUES ('" . $_POST["name"] . "', '" . $_POST["tel"] . "', '" . $_POST["email"] . "', 
+            '" . $_POST["address"] . "', '" . $_POST["url"] . "')";
+        case "update":
+            return "UPDATE xin_feng_branches SET name='" . $_POST["name"] . "', tel='" . $_POST["tel"] . "', email='" . $_POST["email"] . "', 
+            address='" . $_POST["address"] . "', url='" . $_POST["url"] . "' WHERE id='" . $_POST["id"] . "'";
+        case "delete":
+            return "DELETE FROM xin_feng_branches WHERE id='" . $_POST["id"] . "'";
+        default:
+            return;
+    }
+}
+
 // Run given sql and show result.
-function branchRunSql($branchSql)
+function branchRunSqlAndEcho($branchSql)
 {
     include 'db.php';
     if ($conn->query($branchSql) === TRUE) {
