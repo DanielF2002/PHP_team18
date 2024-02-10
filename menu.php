f1dc66be5070557036b0cec777e9bfdca7269e7b
Xingxing Wang <xingxing23000@student.hamk.fi>
2024-02-10 21:53:46 +0200

    completed features

--- menu.php
+++ menu.php
@@ -1,6 +1,7 @@
 <?php
 $pageTitle = "Menu Of Midnight Sun Bistro";
 $pageDescription = "Read the menu";
+$pageCssFilename = "menu";
 $pageAdmin = false;
 include 'layout/header.php';
 include 'db.php';
@@ -19,16 +20,19 @@ if ($resultCategories->num_rows > 0) {
 
 <hr>
 <div class="container">
+    <h1 class="text-center">Explore Menu of Midnight Sun</h1>
     <div class="row">
-        <div class="col-md-2">
-            <h3>Categories</h3>
+        <div class="card col-md-3" id="menu-category">
+        <img class="card-img-top" id="category-img" src="layout/images/img_menu.jpg" alt="food and drink">
+            <p class="card-text">Click on the category to read our menu.</p>
+            <h2 class="card-title">Categories</h2>
             <ul class="nav flex-column list-group">
                 <?php foreach ($categories as $category) : ?>
                     <li class="nav-link list-group-item category-item" data-category="<?php echo $category; ?>"><?php echo $category; ?></li>
                 <?php endforeach; ?>
             </ul>
         </div>
-        <div class="col-md-6" id="menu-items">
+        <div class="col-md-9" id="menu-items">
             <?php
             // Check if the category parameter is set
             if (isset($_GET['category'])) {
@@ -44,11 +48,11 @@ if ($resultCategories->num_rows > 0) {
                 // Output menu items as HTML
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
-                        echo "<div class='menu-item'>";
-                        echo "<h4>" . $row['item_name'] . "</h4>";
-                        echo "<p><strong>Ingredients:</strong> " . $row['ingredients'] . "</p>";
-                        echo "<p><strong>Vegetarian:</strong> " . ($row['vegetarian'] ? 'Yes' : 'No') . "</p>";
-                        echo "<p><strong>Price:</strong> $" . number_format($row['price'], 2) . "</p>";
+                        echo "<div>";
+                        echo "<h4>" . $row['item_name'] . "</h4>" ."<br>";
+                        echo "<p><strong>Ingredients:</strong> " . $row['ingredients'] . "</p>"."<br>";
+                        echo "<p><strong>Vegetarian:</strong> " . ($row['vegetarian'] ? 'Yes' : 'No') . "</p>"."<br>";
+                        echo "<p><strong>Price:</strong> $" . number_format($row['price'], 2) . "</p>"."<br>";
                         echo "</div>";
                     }
                 } else {
@@ -57,14 +61,39 @@ if ($resultCategories->num_rows > 0) {
 
                 // Close the prepared statement
                 $stmt->close();
-            } else {
-                echo "Please click on the category to read our menu.";
+            } 
+            else {
+                echo '
+                <div id="carouselExampleIndicators" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="layout/images/img_menu_display1.jpg" class="d-block w-100" alt="food display">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="layout/images/img_menu_display2.jpg" class="d-block w-100" alt="food display">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="layout/images/img_menu_display3.jpg"" class="d-block w-100" alt="food display">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>';
             }
             ?>
         </div>
-        <div class="col-md-4" id="menu-image">
-            <img class="img-fluid" src="layout/images/img_menu.jpg" alt="menu reading page image" style="border-radius: 1rem;">
-        </div>
+
     </div>
 </div>
 
@@ -74,7 +103,7 @@ if ($resultCategories->num_rows > 0) {
         document.querySelectorAll('.category-item').forEach(item => {
             item.addEventListener('click', function() {
                 var category = this.getAttribute('data-category');
-                window.location.href = 'feature8_menu_user.php?category=' + encodeURIComponent(category);
+                window.location.href = 'menu.php?category=' + encodeURIComponent(category);
             });
         });
     });
--- menu_admin.php
+++ menu_admin.php
@@ -1,19 +1,25 @@
 <?php
 $pageTitle = "Menu Management";
 $pageDescription = "Manage the menu";
+$pageCssFilename = "menu";
 $pageAdmin = true;
 include 'layout/header.php';
 include 'db.php';
 
 $sql = "select * from xingxing_menuItems";
-$result = $conn->query($sql);?>
+$result = $conn->query($sql);
+?>
+<hr>
 <div class="container">
+    <h1>Menu Management</h1>
     <nav class="navbar" id="menu manage">
     <div class="container-fluid">
-        <a href="menu_admin_create.php" class="btn btn-secondary btn-sm" role="button">Add New Item</a>
+        <a href="menu_admin_create.php" class="btn btn-secondary" role="button">Add New Item</a>
         <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="d-flex" role="search">
-        <input class="form-control me-2 " type="search" placeholder="Search by keyword" name="keyword" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+
+            <button class="btn btn-success" type="submit">Search</button>
+            <input class="form-control" type="search" placeholder="Search by keyword" name="keyword" aria-label="Search">
+
         </form>
     </div>
     </nav>
@@ -30,61 +36,64 @@ $result = $conn->query($sql);?>
         $result = $conn->query($sql);
     ?>
 
-    <table class="table table-responsive" id="admin-menu-table">
-        <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Category</th>
-        <th>Ingredients</th>
-        <th>Vegetarian</th>
-        <th>Price</th>
-        <th>Edit</th>
-        <th>Delete</th>
-        </tr>
+    <div class="container" id="admin-menu">
 
-        <?php 
-            if($result ->num_rows > 0) {
-                while($row = $result ->fetch_assoc()){
-        ?>
+        <table class="table table-responsive" id="admin-menu-table">
+            <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Ingredients</th>
+            <th>Vegetarian</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th>Delete</th>
+            </tr>
 
-        <tr>
-        <td><?php echo $row["item_id"]; ?></td>
-        <td><?php echo $row["item_name"]; ?></td>
-        <td><?php echo $row["category"]; ?></td>
-        <td><?php echo $row["ingredients"]; ?></td>
-        <td><?php echo $row["vegetarian"]; ?></td>
-        <td><?php echo $row["price"]; ?></td>
-        <td><a href="menu_admin_update.php?item_id=<?php echo $row['item_id']; ?>" class="btn btn-secondary btn-sm" role="button">Update</a></td>
-        <td><a data-item-id="<?php echo $row['item_id']; ?>" class="btn btn-outline-secondary btn-sm delete-btn" role="button">Delete</a></td>
+            <?php 
+                if($result ->num_rows > 0) {
+                    while($row = $result ->fetch_assoc()){
+            ?>
 
-        </tr>
+            <tr>
+            <td><?php echo $row["item_id"]; ?></td>
+            <td><?php echo $row["item_name"]; ?></td>
+            <td><?php echo $row["category"]; ?></td>
+            <td><?php echo $row["ingredients"]; ?></td>
+            <td><?php echo $row["vegetarian"]; ?></td>
+            <td><?php echo $row["price"]; ?></td>
+            <td><a href="menu_admin_update.php?item_id=<?php echo $row['item_id']; ?>" class="btn btn-secondary btn-sm" role="button">Update</a></td>
+            <td><a data-item-id="<?php echo $row['item_id']; ?>" class="btn btn-outline-secondary btn-sm delete-btn" role="button">Delete</a></td>
 
-    <?php 
-        }
-        }
-        else
-        {
-            echo "no results";
-        }
+            </tr>
 
-
-        // Check if item_id is provided in the URL
-        if(isset($_GET['delete_item_id'])) {
-            $delete_item_id = $_GET['delete_item_id'];
-
-            // Query to delete the menu item
-            $delete_query = "DELETE FROM xingxing_menuItems WHERE item_id = '$delete_item_id'";
-            if(mysqli_query($conn, $delete_query)) {
-                echo '<script>alert("Item deleted successfully."); window.location.href = "'. $_SERVER['PHP_SELF'] .'";</script>';
-            } else {
-                echo '<script>alert("Error deleting item.");</script>';
+            <?php 
             }
-        }
+            }
+            else
+            {
+                echo "no results";
+            }
 
-        $conn->close();
-    ?>
-    </table>
+
+            // Check if item_id is provided in the URL
+            if(isset($_GET['delete_item_id'])) {
+                $delete_item_id = $_GET['delete_item_id'];
+
+                // Query to delete the menu item
+                $delete_query = "DELETE FROM xingxing_menuItems WHERE item_id = '$delete_item_id'";
+                if(mysqli_query($conn, $delete_query)) {
+                    echo '<script>alert("Item deleted successfully."); window.location.href = "'. $_SERVER['PHP_SELF'] .'";</script>';
+                } else {
+                    echo '<script>alert("Error deleting item.");</script>';
+                }
+            }
+
+            $conn->close();
+            ?>
+        </table>
     </div>
+</div>
 
     <script>
     // Function to handle delete button click
