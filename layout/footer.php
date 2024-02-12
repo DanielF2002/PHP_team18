edbac92000bbdacecfde9bd4238445418d4a9405
Xin Feng <126309503+DanielF2002@users.noreply.github.com>
2024-02-12 17:10:35 +0200

    Add validation for feature3, and header footer bugfix.

--- layout/footer.php
+++ layout/footer.php
@@ -25,9 +25,20 @@
             </div>
         </footer>
     </div> <!--container-->
+    <noscript>
+        Your browser does not support JavaScript, or it is disabled. Please check your browser settings.
+    </noscript>
+    <?php
+        if (isset($pageJs) && !empty($pageJs)) {
+            echo '
+                <script src="js/'.$pageJs.'.js"></script>
+            ';
+        }
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
 </body>
 
 </html>
+
